6aaacb89cfd9f68989712d390f181ed8722813cc
Mrau Hu <mrauhu@yandex.ru>
2017-02-27 05:53:45 +0300

    Remove unused function `EMT_safe_tags()` from `plugin.php`. Update plugin version to 0.4.1

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: [ram108] Web Typography Standards
 Plugin URI: http://wordpress.org/plugins/ram108-typo/
 Description: Apply web typography standards to Wordpress content: space control, punctuation, intelligent character replacement, CSS hooks and more.
-Version: 0.4.0
+Version: 0.4.1
 Author: ram108
 Author URI: http://profiles.wordpress.org/ram108
 Author Email: plugin@ram108.ru
@@ -37,33 +37,6 @@ function EMT_run( $text ){
 	global $ram108_typo;
 	$ram108_typo->set_text( $text );
 	return $ram108_typo->apply();
-}
-
-/**
- * Change EMT safe tags
- * with checking result of filter 'no_texturize_tags'
- * @global EMTypograph $ram108_typo
- * @param array $old Tags list before run filter
- * @param array $new Tags list after run filter
- */
-function EMT_safe_tags ( $old, $new ) {
-	global $ram108_typo;
-	
-	// Check for changes
-	$added_tags = array_diff( $old, $new );
-	$removed_tags = array_diff( $new, $old );
-	
-	if ( count( $added_tags ) > 0 ) {
-		foreach( $added_tags as $tag_to_add ) {
-			$ram108_typo->add_safe_tag($tag_to_add);
-		}
-	}
-	
-	if ( count( $removed_tags ) > 0 ) {
-		foreach( $removed_tags as $tag_to_remove ) {
-			$ram108_typo->remove_safe_block( $tag_to_remove );
-		}
-	}
 }
 
 /**
