801dcf0dc26662651085ed937973f2039f49966b
Mrau Hu <mrauhu@yandex.ru>
2017-02-27 15:33:56 +0300

    Fixed: wrong breaking string into pieces in function `EMT_wptexturize()`.
    Revert update f87a341. Downgrade `EMT_wptexturize()` to version `0.4.0`.

--- EMT_wptexturize.php
+++ EMT_wptexturize.php
@@ -21,16 +21,11 @@
  *
  * Code within certain html blocks are skipped.
  *
- * Do not use this function before the {@see 'init'} action hook; everything will break.
- *
- * @since 0.71
  *
  * @global array $wp_cockneyreplace Array of formatted entities for certain common phrases
  * @global array $shortcode_tags
  * @staticvar array $static_characters
  * @staticvar array $static_replacements
- * @staticvar array $dynamic_characters
- * @staticvar array $dynamic_replacements
  * @staticvar array $default_no_texturize_tags
  * @staticvar array $default_no_texturize_shortcodes
  * @staticvar bool  $run_texturize
@@ -41,11 +36,8 @@
  */
 function EMT_wptexturize( $text, $reset = false ) {
 	global $wp_cockneyreplace, $shortcode_tags;
-	static $static_characters = null,
-	$static_replacements = null,
-	$default_no_texturize_tags = null,
-	$default_no_texturize_shortcodes = null,
-	$run_texturize = true;
+	static $static_characters, $static_replacements,
+		$default_no_texturize_tags, $default_no_texturize_shortcodes, $run_texturize = true;
 
 	// If there's nothing to do, just stop.
 	if ( empty( $text ) || false === $run_texturize ) {
@@ -55,7 +47,7 @@ function EMT_wptexturize( $text, $reset = false ) {
 	// Set up static variables. Run once only.
 	if ( $reset || ! isset( $static_characters ) ) {
 		/**
-		 * Filters whether to skip running wptexturize().
+		 * Filter whether to skip running wptexturize().
 		 *
 		 * Passing false to the filter will effectively short-circuit wptexturize().
 		 * returning the original text passed to the function instead.
@@ -90,16 +82,18 @@ function EMT_wptexturize( $text, $reset = false ) {
 
 	// Must do this every time in case plugins use these filters in a context sensitive manner
 	/**
-	 * Filters the list of HTML elements not to texturize.
+	 * Filter the list of HTML elements not to texturize.
 	 *
 	 * @since 2.8.0
 	 *
 	 * @param array $default_no_texturize_tags An array of HTML element names.
 	 */
 	$no_texturize_tags = apply_filters( 'no_texturize_tags', $default_no_texturize_tags );
-	
+
+	#EMT_safe_tags( $default_no_texturize_shortcodes, $no_texturize_tags );
+
 	/**
-	 * Filters the list of shortcodes not to texturize.
+	 * Filter the list of shortcodes not to texturize.
 	 *
 	 * @since 2.8.0
 	 *
@@ -107,40 +101,49 @@ function EMT_wptexturize( $text, $reset = false ) {
 	 */
 	$no_texturize_shortcodes = apply_filters( 'no_texturize_shortcodes', $default_no_texturize_shortcodes );
 
-	$no_texturize_tags_stack = array();
 	$no_texturize_shortcodes_stack = array();
 
 	// Look for shortcodes and HTML elements.
 
-	preg_match_all( '@\[/?([^<>&/\[\]\x00-\x20=]++)@', $text, $matches );
-	$tagnames = array_intersect( array_keys( $shortcode_tags ), $matches[1] );
-	$found_shortcodes = ! empty( $tagnames );
-	$shortcode_regex = $found_shortcodes ? _get_wptexturize_shortcode_regex( $tagnames ) : '';
-	$regex = _get_wptexturize_split_regex( $shortcode_regex );
+	$tagnames = array_keys( $shortcode_tags );
+	$tagregexp = join( '|', array_map( 'preg_quote', $tagnames ) );
+	$tagregexp = "(?:$tagregexp)(?![\\w-])"; // Excerpt of get_shortcode_regex().
+
+	$comment_regex =
+		  '!'           // Start of comment, after the <.
+		. '(?:'         // Unroll the loop: Consume everything until --> is found.
+		.     '-(?!->)' // Dash not followed by end of comment.
+		.     '[^\-]*+' // Consume non-dashes.
+		. ')*+'         // Loop possessively.
+		. '(?:-->)?';   // End of comment. If not found, match all input.
+
+	$shortcode_regex =
+		  '\['              // Find start of shortcode.
+		. '[\/\[]?'         // Shortcodes may begin with [/ or [[
+		. $tagregexp        // Only match registered shortcodes, because performance.
+		. '(?:'
+		.     '[^\[\]<>]+'  // Shortcodes do not contain other shortcodes. Quantifier critical.
+		. '|'
+		.     '<[^\[\]>]*>' // HTML elements permitted. Prevents matching ] before >.
+		. ')*+'             // Possessive critical.
+		. '\]'              // Find end of shortcode.
+		. '\]?';            // Shortcodes may end with ]]
+
+	$regex =
+		  '/('                   // Capture the entire match.
+		.     $shortcode_regex   // Find shortcodes.
+		. ')/s';
 
 	$textarr = preg_split( $regex, $text, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY );
 
 	foreach ( $textarr as &$curl ) {
 		// Only call _wptexturize_pushpop_element if $curl is a delimiter.
 		$first = $curl[0];
-		if ( '<' === $first ) {
-			if ( '<!--' === substr( $curl, 0, 4 ) ) {
-				// This is an HTML comment delimiter.
-				continue;
-			} else {
-				// This is an HTML element delimiter.
-
-				// Replace each & with &#038; unless it already looks like an entity.
-				$curl = preg_replace( '/&(?!#(?:\d+|x[a-f0-9]+);|[a-z1-4]{1,8};)/i', '&#038;', $curl );
-
-				_wptexturize_pushpop_element( $curl, $no_texturize_tags_stack, $no_texturize_tags );
-			}
-
-		} elseif ( '' === trim( $curl ) ) {
+		if ( '' === trim( $curl ) ) {
 			// This is a newline between delimiters.  Performance improves when we check this.
 			continue;
 
-		} elseif ( '[' === $first && $found_shortcodes && 1 === preg_match( '/^' . $shortcode_regex . '$/', $curl ) ) {
+		} elseif ( '[' === $first && 1 === preg_match( '/^' . $shortcode_regex . '$/', $curl ) ) {
 			// This is a shortcode delimiter.
 
 			if ( '[[' !== substr( $curl, 0, 2 ) && ']]' !== substr( $curl, -2 ) ) {
@@ -151,7 +154,7 @@ function EMT_wptexturize( $text, $reset = false ) {
 				continue;
 			}
 
-		} elseif ( empty( $no_texturize_shortcodes_stack ) && empty( $no_texturize_tags_stack ) ) {
+		} elseif ( empty( $no_texturize_shortcodes_stack ) ) {
 			// This is neither a delimiter, nor is this content inside of no_texturize pairs.  Do texturize.
 
 			$curl = str_replace( $static_characters, $static_replacements, $curl );
